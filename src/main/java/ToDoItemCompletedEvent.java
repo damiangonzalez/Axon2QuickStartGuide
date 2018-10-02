20ffa63da61888245dd096b183726962b154af35
DamianGo <damian_gonzalez@yahoo.com>
2018-10-02 18:52:12 -0400

    Initial commit of working files for quickstart

---
+++ src/main/java/ToDoItemCompletedEvent.java
@@ -0,0 +1,12 @@
+public class ToDoItemCompletedEvent {
+
+  private final String todoId;
+
+  public ToDoItemCompletedEvent(String todoId) {
+    this.todoId = todoId;
+  }
+
+  public String getTodoId() {
+    return todoId;
+  }
+}
